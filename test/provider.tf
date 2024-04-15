53a10d66ad93e6e9de72c384b00b66b29f873314
dhille98 <dhillevajja1@gmail.com>
2024-04-15 15:00:51 +0530

    dd

--- test/provider.tf
+++ test/provider.tf
@@ -1,6 +1,6 @@
 
 provider "google" {
-  credentials = file(home/dell/acoustic-cargo-416714-c04b9fca2f30.json)
+  credentials = file("/home/dell/acoustic-cargo-416714-c04b9fca2f30.json")
   project     = "acoustic-cargo-416714"
   region      = "us-central1-a"
 }
